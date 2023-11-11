01c3a9886505f891e14d518393ec5f61472285b5
Sandeep Gaur <sandeepgaur98223@gmail.com>
2023-11-11 15:38:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './todo';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'youtube-ang';
+  title = 'First Angular Project';
+  // name='Sandeep Gaur';
+  // date=new Date();
+
+  // constructor(){
+  //   console.log("constructor called!!!")
+  // }
+  todoValue!: string;
+  list!: Todo[];
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+
+  addItem(){
+    if(this.todoValue!="")
+    {
+      const newItem: Todo ={
+        id: Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue="";
+  }
+
+  deleteItem(id:number){
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+
+  
 }
